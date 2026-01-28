aadd7002dd8ef39daa2d61792b4d93e8dc5fe8ff
Ilham Ramdhani <ramdhaniit@gmail.com>
2026-01-28 07:24:28 +0000

    upadte UOM production

--- next.config.ts
+++ next.config.ts
@@ -1,23 +1,35 @@
+// import type { NextConfig } from "next";
+
+// const nextConfig: NextConfig = {
+//   images: {
+//     remotePatterns: [
+//       {
+//         protocol: "https",
+//         hostname: "api-ekaplus.ekatunggal.com",
+//         // port: "8000",
+//         pathname: "/files/**",
+//       },
+//     ],
+//     // remotePatterns: [
+//     //   {
+//     //     protocol: "http",
+//     //     hostname: "192.168.101.214",
+//     //     port: "8000",
+//     //     pathname: "/files/**",
+//     //   },
+//     // ],
+//   },
+// };
+
+// export default nextConfig;
+
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
+  output: "export",
+  trailingSlash: true,
   images: {
-    remotePatterns: [
-      {
-        protocol: "https",
-        hostname: "api-ekaplus.ekatunggal.com",
-        // port: "8000",
-        pathname: "/files/**",
-      },
-    ],
-    // remotePatterns: [
-    //   {
-    //     protocol: "http",
-    //     hostname: "192.168.101.214",
-    //     port: "8000",
-    //     pathname: "/files/**",
-    //   },
-    // ],
+    unoptimized: true,
   },
 };
 
